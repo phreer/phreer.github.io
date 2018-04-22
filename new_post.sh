a280eebdb967af272560685a604d94ac271d4be3
Phree <iphreeliu@gmail.com>
2018-04-22 16:57:04 +0800

    new post at Sun Apr 22 16:57:04 DST 2018

--- new_post.sh
+++ new_post.sh
@@ -4,4 +4,4 @@ git commit -m "new post at $(date)"
 git push origin master
 git checkout coding-pages
 git merge master
-git push coidng-net-pages coding-pages
+git push coding-net-pages coding-pages
